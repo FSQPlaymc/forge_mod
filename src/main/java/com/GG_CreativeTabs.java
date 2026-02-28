7974e9a10c3985225f15b0ae5298ce643120f78c
FSQPlaymc <13077977318@163.com>
2026-03-01 03:13:55 +0800

    new

--- src/main/java/com/GG_CreativeTabs.java
+++ src/main/java/com/GG_CreativeTabs.java
@@ -1,9 +1,20 @@
 package com;
 
+import com.MaaaMod.MaaaMod;
+import com.items.GG_Items;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod.EventBusSubscriber(modid = MaaaMod.MODID)
 
 public class GG_CreativeTabs {
     public static final CreativeTabs GG_1=new CreativeTabs(CreativeTabs.getNextID(),"fsqwmc_1") {
@@ -12,4 +23,9 @@ public class GG_CreativeTabs {
             return new ItemStack(Item.getItemFromBlock(Blocks.TNT));
         }
     };
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public static void modles_items(ModelRegistryEvent a){
+        ModelLoader.setCustomModelResourceLocation(GG_Items.a,0,new ModelResourceLocation(GG_Items.a.getRegistryName(),"inventory"));
+    }
 }
--- src/main/java/com/MaaaMod/MaaaMod.java
+++ src/main/java/com/MaaaMod/MaaaMod.java
@@ -1,5 +1,6 @@
 package com.MaaaMod;
 
+import com.TC.TC2;
 import com.TC.TCcs;
 import com.items.GG_Items;
 import net.minecraft.init.Blocks;
@@ -8,8 +9,9 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+import slimeknights.tconstruct.library.MaterialIntegration;
 import slimeknights.tconstruct.library.TinkerRegistry;
-import slimeknights.tconstruct.library.materials.Material;
+import slimeknights.tconstruct.library.materials.*;
 
 @Mod(
         modid = MaaaMod.MODID, name = MaaaMod.NAME, version = MaaaMod.VERSION,
@@ -17,7 +19,9 @@ import slimeknights.tconstruct.library.materials.Material;
 )
 public class MaaaMod
 {
-    public static final String MODID = "maaamod";
+    public static Material TCa;
+
+    public static final String MODID = "assets/maaamod";
     public static final String NAME = "Exa Mod";
     public static final String VERSION = "1.0";
 
@@ -27,6 +31,24 @@ public class MaaaMod
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+        TCa=new Material("TCa",Integer.parseInt("7AFF83", 16)){{
+            this.setCraftable(true);
+            this.addCommonItems("aaa");
+            this.addItem(GG_Items.a,1,144);
+            this.addTrait(new TCcs());
+            this.addTrait(new TC2("TC2",Integer.parseInt("7AFF83", 16)));//Integer.parseInt("0x88FF88", 16)把16进制转为rgb输入
+            this.setRepresentativeItem(GG_Items.a);
+        }};
+        TinkerRegistry.addMaterialStats(TCa, new BowMaterialStats(0.5F, 1.5F, 7.0F));
+        TinkerRegistry.addMaterialStats(TCa, new HeadMaterialStats(204, 6.0F, 6.0F, 2), new IMaterialStats[]{new HandleMaterialStats(0.85F, 60), new ExtraMaterialStats(50)});
+        // 3. 添加材料统计数据
+        MaterialIntegration integration = new MaterialIntegration(TCa);
+        // 1. 创建材料实例
+        // 参数说明：材料名称(唯一标识符)，颜色(十六进制)
+        //在这里按照这个格式为游戏注册词条
+        //TraitCorpseMountain()可以换成其他你任意想要注册词条的构建方法，如果构建方法存在参数，那你还需要填上参数
+        TinkerRegistry.addMaterial(TCa);
+        TinkerRegistry.integrate(integration);
         //TinkerRegistry.addTrait(new TCcs());
         // 1. 创建材料实例
         // 参数说明：材料名称(唯一标识符)，颜色(十六进制)
