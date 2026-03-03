cc156c6460fe9856cf5eee674bc2b244d3ab902a
FSQPlaymc <13077977318@163.com>
2026-03-03 21:42:35 +0800

    new

---
+++ src/main/java/com/TC/MyTMaterials.java
@@ -0,0 +1,9 @@
+package com.TC;
+
+import slimeknights.tconstruct.library.materials.Material;
+
+public class MyTMaterials extends Material {
+    public MyTMaterials(String identifier, String color) {//color16进制色码不要'#'号
+        super(identifier, Integer.parseInt(color, 16));
+    }
+}
--- src/main/java/com/TC/TMaterials_set.java
+++ src/main/java/com/TC/TMaterials_set.java
@@ -3,7 +3,11 @@ package com.TC;
 import slimeknights.tconstruct.library.materials.Material;
 
 public class TMaterials_set {
-    public static Material TCa;
+    public static Material TCb;
 
-    public TMaterials_set(){}
+    public TMaterials_set(){
+        TCb=new MyTMaterials("666","e3fa50"){{
+            //addItem();
+        }};
+    }
 }
