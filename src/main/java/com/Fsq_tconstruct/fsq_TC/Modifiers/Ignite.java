60befd3615ddc3ea731fd4bc0d2546b8506eaeca
huawei <13077977318@163.com>
2026-07-08 23:22:26 +0800

    add

--- src/main/java/com/Fsq_tconstruct/fsq_TC/Modifiers/Ignite.java
+++ src/main/java/com/Fsq_tconstruct/fsq_TC/Modifiers/Ignite.java
@@ -1,4 +1,17 @@
 package com.Fsq_tconstruct.fsq_TC.Modifiers;
 
-public class Ignite {
+import net.minecraft.world.entity.LivingEntity;
+import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.hook.combat.MeleeHitModifierHook;
+import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+
+public class Ignite extends Modifier implements MeleeHitModifierHook {
+    public void afterMeleeHit(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float damageDealt) {
+        LivingEntity a= context.getLivingTarget();
+        if (a != null) {
+            a.setSecondsOnFire(5);
+        }
+    }
 }
