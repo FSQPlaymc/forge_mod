eee82cf1af5f1a102ddb1867389b743515d281f4
FSQPlaymc <13077977318@163.com>
2026-08-08 00:58:20 +0800

    new

--- src/main/java/com/Fsq_tconstruct/fsq_TC/Modifiers/Undying.java
+++ src/main/java/com/Fsq_tconstruct/fsq_TC/Modifiers/Undying.java
@@ -1,31 +1,30 @@
 package com.Fsq_tconstruct.fsq_TC.Modifiers;
 
-
-import net.minecraft.client.renderer.texture.atlas.SpriteSourceType;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.modifiers.hook.armor.DamageBlockModifierHook;
-import slimeknights.tconstruct.library.modifiers.hook.armor.ModifyDamageModifierHook;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
-import java.util.Objects;
+public class Undying extends Modifier implements DamageBlockModifierHook {
+    @Override
+    protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
+        super.registerHooks(hookBuilder);
+        hookBuilder.addHook(this, ModifierHooks.DAMAGE_BLOCK);
+    }
 
-public class Undying extends Modifier implements  DamageBlockModifierHook {//不灭
     public boolean isDamageBlocked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount){
         if (source.is(DamageTypeTags.IS_FIRE)){
-            LivingEntity PLAYER= context.getEntity();
-            PLAYER.setSecondsOnFire(100);
+            LivingEntity entity = context.getEntity();
+            entity.setSecondsOnFire(100);
             return true;
         }
         return false;
-        //return amount;
     }
 }
